df50280e8ee97d1d71e04446a520662d88ff70dc
Luka Devic <luka.m.devic@gmail.com>
2018-11-08 11:05:37 +0100

    added new feature (Add)

---
+++ GitCommandsTestWithConsoleApp/AddMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCommandsTestWithConsoleApp
+{
+    class AddMethod
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
